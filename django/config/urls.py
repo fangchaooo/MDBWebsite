e27a9cf90446a9ee8544af30fdf3cb21995dd5aa
fangchaooo <fc_btby@hotmail.com>
2018-05-30 18:10:38 +0800

    core app

---
+++ django/config/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.contrib import admin
+
+import core.urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(core.urls, namespace='core')),
+]
